4323de728473bbaee08842fee9eae365bfc4c1e1
Bharani9361 <bharanidharan@eparampara.org>
2026-02-19 14:39:20 +0530

    Delete with-region-admin-src/Share to bala directory

--- with-region-admin-src/Share to bala/src/pages/Admin_ui/contexts/useSocket.js
+++
@@ -1,9 +0,0 @@
-import { useContext } from 'react';
-import { SocketContext } from './SocketContext';
-
-const useSocket = () => {
-  const { socket, data } = useContext(SocketContext);
-  return { socket, data };
-};
-
-export default useSocket;
