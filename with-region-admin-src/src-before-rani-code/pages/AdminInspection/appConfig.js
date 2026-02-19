47ac9be0db7b68fd4a023c121f14ac2c4bc54856
Bharani9361 <bharanidharan@eparampara.org>
2026-02-19 14:39:34 +0530

    Delete with-region-admin-src/src-before-rani-code directory

--- with-region-admin-src/src-before-rani-code/pages/AdminInspection/appConfig.js
+++
@@ -1,5 +0,0 @@
-
-const webCamResolution = { width: 640, height: 480 };
-// const webCamResolution = { width: 1440, height: 1080 };
-
-export { webCamResolution };
--- with-region-admin-src/src-before-rani-code/pages/Admin_ui/Features/demo.js
+++
@@ -1,4 +0,0 @@
-
-// to find node memory allocated for your project
-const v8 = require('v8');
-console.log(`Max heap size (in MB): ${v8.getHeapStatistics().heap_size_limit / 1024 / 1024}`);
